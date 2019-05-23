3a5bafae66af9b4bcd16c1dd25a9e7c26bd9b8f2
Khaled Hossain <md.khaled.eee@gmail.com>
2019-05-23 12:10:53 +0600

    add navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,38 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from "react"
+import { Link } from "gatsby"
+
+// import './header.module.scss';
+import { navList, navItem, header, title, activeNavItem } from "./header.module.scss";
 
 const Header = () => {
   return (
-    <header>
-      <h1>khaled hossain</h1>
+    <header className={header}>
+      <h1>
+        <Link className={title} to="/">
+          khaled hossain
+        </Link>
+      </h1>
       <nav>
-        <ul>
+        <ul className={navList}>
           <li>
-            <Link to="/">Home</Link>
+            <Link className={navItem} activeClassName={activeNavItem} to="/">
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/blog">Blog</Link>
+            <Link className={navItem} activeClassName={activeNavItem} to="/blog" >
+              Blog
+            </Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link className={navItem} activeClassName={activeNavItem} to="/about">
+              About
+            </Link>
           </li>
           <li>
-            <Link to="/contact">Contact</Link>
+            <Link className={navItem} activeClassName={activeNavItem} to="/contact">
+              Contact
+            </Link>
           </li>
         </ul>
       </nav>
@@ -25,4 +40,4 @@ const Header = () => {
   )
 }
 
-export default Header;
+export default Header
